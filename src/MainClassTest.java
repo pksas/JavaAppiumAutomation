291802f4b4af67dee2a676d69a3dd6843a3d4721
pksas <plsas82@gmail.com>
2020-03-22 21:41:55 +0300

    Написан Тест2 (testGetClassNumber).

--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -1,3 +1,4 @@
+import org.junit.Assert;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -11,5 +12,13 @@ public class MainClassTest
         int expected = 14;
 
         assertEquals("The result of the getLocalNumber method is not equal to 14", expected, actual);
+    }
+
+    @Test
+    public void testGetClassNumber()
+    {
+        int actual = new MainClass().getClassNumber();
+
+        Assert.assertTrue("The result of the getClassNumber method is not bigger than 45", actual > 45);
     }
 }
